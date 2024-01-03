5f41bfbd3a0e55790e417bcfee2b4053088a45ff
Alberto Rosas <alberto.e@incfile.com>
2024-01-02 21:31:50 -0800

    Fix: app.css not compiling through Vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.ts',
+            input: [
+                'resources/js/app.ts',
+                'resources/css/app.css',
+            ],
             refresh: true,
         }),
         vue({
