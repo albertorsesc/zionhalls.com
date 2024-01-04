602fba7c89eaf555c4fa7a5aa7b6393eb3f53f61
Alberto Rosas <alberto.e@incfile.com>
2024-01-03 21:41:29 -0800

    fix: nullable property safety

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -50,7 +50,7 @@ const showingNavigationDropdown = ref(false);
                     <div class="hidden sm:ml-6 sm:block">
                         <div class="flex items-center">
                             <div class="relative ml-3">
-                                <div v-if="! $page.props.auth.user.id">
+                                <div v-if="! $page.props.auth.user?.id">
                                     <a href="/register" class="rounded-md px-3 py-2 text-sm font-medium text-white">Register</a>
                                     <a href="/login" class="rounded-md px-3 py-2 text-sm font-medium text-white">Log in</a>
                                 </div>
